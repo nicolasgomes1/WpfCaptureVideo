9ee6bf5feb194d1d5bc9c08262aa03976f8b0b13
nicol <nicolasdiasgomes@gmail.com>
2025-07-29 23:54:42 +0200

    fghgfh

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -8,11 +8,23 @@ namespace WpfRecorder.ViewModels;
 public partial class MainWindowViewModel : ObservableObject
 {
     private readonly ILogger _logger = Log.ForContext<MainWindowViewModel>();
+
+    [ObservableProperty] public partial bool IsStartButtonEnabled { get; set; } = true;
+    [ObservableProperty] public partial bool IsPauseButtonEnabled { get; set; } = false;
+    [ObservableProperty] public partial bool IsResumeButtonEnabled { get; set; } = false;
+    [ObservableProperty] public partial bool IsSaveButtonEnabled { get; set; } = false;
+    [ObservableProperty] public partial bool IsCancelButtonEnabled { get; set; } = false;
+
     
     [RelayCommand]
     private async Task StartRecording()
     {
         _logger.Information("Start recording");
+        IsStartButtonEnabled = false;
+        IsResumeButtonEnabled = false;
+        IsPauseButtonEnabled = true;
+        IsSaveButtonEnabled = true;
+        IsCancelButtonEnabled = true;
         await Task.CompletedTask;
     }
     
@@ -20,6 +32,10 @@ public partial class MainWindowViewModel : ObservableObject
     private async Task PauseRecording()
     {
         _logger.Information("Pause recording");
+        IsResumeButtonEnabled = true;
+        IsStartButtonEnabled = false;
+        IsPauseButtonEnabled = false;
+
         await Task.CompletedTask;
     }
     
@@ -27,6 +43,9 @@ public partial class MainWindowViewModel : ObservableObject
     private async Task ResumeRecording()
     {
         _logger.Information("Resume recording");
+        IsSaveButtonEnabled = true;
+        IsResumeButtonEnabled = false;
+        IsPauseButtonEnabled = true;
         await Task.CompletedTask;
     }
     
@@ -34,6 +53,11 @@ public partial class MainWindowViewModel : ObservableObject
     private async Task SaveRecording()
     {
         _logger.Information("Save recording");
+        IsStartButtonEnabled = true;
+        IsResumeButtonEnabled = false;
+        IsSaveButtonEnabled = false;
+        IsPauseButtonEnabled = false;
+        IsCancelButtonEnabled = false;
         await Task.CompletedTask;
     }
     
@@ -41,6 +65,11 @@ public partial class MainWindowViewModel : ObservableObject
     private async Task CancelRecording()
     {
         _logger.Information("Cancel recording");
+        IsStartButtonEnabled = true;
+        IsPauseButtonEnabled = false;
+        IsResumeButtonEnabled = false;
+        IsSaveButtonEnabled = false;
+        IsCancelButtonEnabled = false;
         await Task.CompletedTask;
     }
     
